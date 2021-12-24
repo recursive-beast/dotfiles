17f49ce3ed36005cb55e949db0842007f3eabd4a
recursive-beast <splentercell.1997@gmail.com>
2021-12-24 10:16:52 +0100

    remove hyphen separator in _LAST_COMMAND_INDICATOR_

--- dot_git-prompt-colors.sh
+++ dot_git-prompt-colors.sh
@@ -3,6 +3,7 @@ override_git_prompt_colors() {
 	GIT_PROMPT_THEME_NAME="Single_line"
 
 	GIT_PROMPT_STAGED="${Red}● ${ResetColor}"
+	GIT_PROMPT_COMMAND_FAIL="${Red}✘ _LAST_COMMAND_STATE_"
 
 	GIT_PROMPT_START_USER="_LAST_COMMAND_INDICATOR_ ${Green}\u${White}@${Magenta}\h${ResetColor} ${BoldYellow}${PathShort}${ResetColor}"
 	GIT_PROMPT_START_ROOT="_LAST_COMMAND_INDICATOR_ ${BoldRed}\u${White}@${Magenta}\h${ResetColor} ${BoldYellow}${PathShort}${ResetColor}"
