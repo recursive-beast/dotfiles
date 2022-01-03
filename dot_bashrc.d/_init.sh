88b6c1721dd6860eac43b9997b257be48f8f7356
recursive-beast <splentercell.1997@gmail.com>
2022-01-03 09:24:41 +0100

    use homebrew's vim instead of system vim

--- dot_bashrc.d/_init.sh
+++ dot_bashrc.d/_init.sh
@@ -85,7 +85,7 @@ export PATH="$PATH:$HOME/.local/bin"
 
 # colored GCC warnings and errors
 #export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
-export EDITOR=$(which vim)
+export EDITOR=vim
 export MAIL=splentercell.1997@gmail.com
 
 # setup bash completion for chezmoi.
