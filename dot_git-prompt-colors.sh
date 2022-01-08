0e10a82d4660487c972f9ffc323e4e511b06278a
recursive-beast <splentercell.1997@gmail.com>
2022-01-08 21:17:13 +0100

    customize bash-git-prompt git info section

--- dot_git-prompt-colors.sh
+++ dot_git-prompt-colors.sh
@@ -2,7 +2,14 @@
 override_git_prompt_colors() {
 	GIT_PROMPT_THEME_NAME="Single_line"
 
-	GIT_PROMPT_STAGED="${Red}● ${ResetColor}"
+	GIT_PROMPT_SEPARATOR=""
+	GIT_PROMPT_STAGED="|${Green}● "
+	GIT_PROMPT_CONFLICTS="|${Magenta}✖ "
+	GIT_PROMPT_CHANGED="|${Red}✚ "
+	GIT_PROMPT_UNTRACKED="|${Cyan}… "
+	GIT_PROMPT_STASHED="|${BoldBlue}⚑ "
+	GIT_PROMPT_CLEAN="|${BoldGreen}✔"
+
 	GIT_PROMPT_COMMAND_FAIL="${Red}✘ _LAST_COMMAND_STATE_"
 
 	GIT_PROMPT_START_USER="_LAST_COMMAND_INDICATOR_ ${Green}\u${White}@${Magenta}\h${ResetColor} ${BoldYellow}${PathShort}${ResetColor}"
