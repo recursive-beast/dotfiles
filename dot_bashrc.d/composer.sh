65857d66e1f67fe9766cddfe51fda3840b575ec2
recursive-beast <splentercell.1997@gmail.com>
2022-04-19 16:22:30 +0000

    drop bashrc modules pattern for a setup script

--- dot_bashrc.d/composer.sh
+++
@@ -1,5 +0,0 @@
-if { command -v php && ! command -v composer; } &>/dev/null; then
-	curl -sS https://getcomposer.org/installer | php || BASHRC_SHOULD_CLEAR=false
-	mv composer.phar ~/.local/bin/composer || BASHRC_SHOULD_CLEAR=false
-	composer config -g bin-dir ~/.local/bin
-fi
